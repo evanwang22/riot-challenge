7d12136b94aad0fa4ef7c0c5c4d439c79acfb67a
Evan Wang <evan.wang22@gmail.com>
2015-08-20 13:28:28 -0400

    View JSON data on the /data endpoint [finishes #101345778]

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Riot Challenge' });
+});
+
+router.get('/data', function(req, res, next) {
+  var matchData = db.get('match_data');
+
+  matchData.find({}, function(err, results){
+    res.json({ matchData: results });
+  });
 });
 
 module.exports = router;
