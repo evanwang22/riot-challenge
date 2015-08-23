f695ef7e606d76966ce22b17b659bbeacb77be0c
Evan Wang <evan.wang22@gmail.com>
2015-08-23 18:54:22 -0400

    Add connection to production db when using cfapps [delivers #101345778]

--- app/db.js
+++ app/db.js
@@ -1,4 +1,10 @@
 var monk = require('monk');
-var db = monk('localhost:27017/riot_challenge');
 
+var uri = 'localhost:27017/riot_challenge'
+if (process.env["VCAP_SERVICES"]) {
+  var vcap_services = JSON.parse(process.env["VCAP_SERVICES"]);
+  uri = vcap_services.mongolab[0].credentials.uri;
+}
+
+var db = monk(uri);
 module.exports = db;
