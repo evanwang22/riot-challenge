a40e0fd121229e89cb41ab8df8538930c9ca1477
Evan Wang <evan.wang22@gmail.com>
2015-08-29 18:29:39 -0400

    Add lodash

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../db');
+var _ = require('lodash');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
